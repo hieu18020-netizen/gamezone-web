17a50aa4af718b40132f178544e10f25af45730b
hieu18020-netizen <hieu.18020@gmail.com>
2026-09-04 13:19:04 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // Thay đổi địa chỉ IP / URL của backend tại đây
 window.APP_CONFIG = {
-  API_URL: "https://zones-economics-challenges-ferrari.trycloudflare.com/api"
+  API_URL: "https://princeton-reported-checking-handhelds.trycloudflare.com/api"
 };
