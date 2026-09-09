24cbd4aac5e6fbc9a0d6c3b1f775705604a57fce
hieu18020-netizen <hieu.18020@gmail.com>
2026-09-09 09:11:00 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // Thay đổi địa chỉ IP / URL của backend tại đây
 window.APP_CONFIG = {
-  API_URL: "https://parliament-howto-wal-seeds.trycloudflare.com/api"
+  API_URL: "https://animated-sie-boston-fda.trycloudflare.com/api"
 };
